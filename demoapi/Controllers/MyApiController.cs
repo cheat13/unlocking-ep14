044f94ecf8895565b1f3a752653fbe8b2811e1e6
Unlocking the future code <live@unlocking.com>
2018-06-14 15:17:49 +0700

    Create simple API.
    Signed-off-by: Unlocking the future code <live@unlocking.com>

---
+++ demoapi/Controllers/MyApiController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using demoapi.Models;
+
+namespace demoapi.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class MyApiController : Controller
+    {
+        [HttpGet]
+        public string Greeting()
+        {
+            return "Hello world";
+        }
+
+        [HttpGet("{name}")]
+        public string Greeting(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
